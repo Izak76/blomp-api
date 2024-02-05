a3b7c2fffdc128e49d9349a11d70a3facde81dd6
Izak <izakalvesgama@outlook.com>
2024-02-05 11:46:10 -0300

    Info properties for Blomp class

--- src/blomp.py
+++ src/blomp.py
@@ -19,8 +19,56 @@ class Blomp:
             raise ConnectionRefusedError("Login returned status code {}".format(p.status_code))
         
         content = next(p.iter_content(8192, True))
-        self.__ss.token = re.findall('<meta name="csrf_token" content="(.+)" />', content)[0]
+        self.__ss.token = re.findall(r'<meta name="csrf_token" content="(.+)" />', content)[0]
         self.__ss.client_id = int(re.findall(r'<input type="hidden" id="clientId" value="(\d+)">', content)[0])
+
+        index_page = self.__ss.get("https://dashboard.blomp.com/dashboard/index").text
+        blomp_info = dict(
+            used_storage = re.search(r'<h5>Used Storage: (.+)</h5>', index_page),
+            avaliable_storage = re.search(r'<h5>Available Storage: (.+)</h5>', index_page),
+            storage_capacity = re.search(r'Storage Capacity:\n(.+)</h5>', index_page),
+            shared_files = re.search(r'<h5>Shared Files: (.+)</h5>', index_page),
+            files_and_folders = re.search(r'<h5>Files & Folders: (.+)</h5>', index_page)
+        )
+
+        for attr, match in blomp_info.items():
+            if match:
+                setattr(self, "__"+attr, match.groups()[0])
+    
+    @property
+    def avaliable_storage(self) -> str|None:
+        return getattr(self, "__avaliable_storage", None)
+    
+    @property
+    def files_and_folders(self) -> int|None:
+        ff:str|None = getattr(self, "__files_and_folders", None)
+
+        if isinstance(ff, str):
+            return int(ff)
+        
+        return None
+    
+    @property
+    def shared_files(self) -> int|None:
+        ff:str|None = getattr(self, "__shared_files", None)
+
+        if isinstance(ff, str):
+            return int(ff)
+        
+        return None
+    
+    @property
+    def storage_capacity(self) -> str|None:
+        ff:str|None = getattr(self, "__storage_capacity", None)
+
+        if isinstance(ff, str):
+            return ff.strip()
+        
+        return None
+    
+    @property
+    def used_storage(self) -> str|None:
+        return getattr(self, "__used_storage", None)
     
     def get_root_directory(self) -> Folder:
         return Folder("", None, self.__ss)
