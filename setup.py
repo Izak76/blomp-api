f14962c388ed140c4858e832dcbae522ffb734d9
Izak <izakalvesgama@outlook.com>
2024-02-20 09:48:27 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 
-with open("requirements.txt") as req, open("README.md") as readme:
+with open("requirements.txt") as req, open("README.md", encoding='utf-8') as readme:
     requirements = req.read().split()
     long_description = readme.read()
 
 
 setup(
     name="blomp_api",
-    version="1.0.1",
+    version="1.0.1.post1",
     author="Izak76",
     url="https://pypi.org/project/blomp-api",
     description="A unofficial Python API client to the Blomp cloud.",
@@ -37,5 +37,11 @@ setup(
     install_requires=requirements,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    python_requires=">=3.8"
+    python_requires=">=3.8",
+    project_urls={
+        "Documentation": "https://github.com/Izak76/blomp-api/blob/main/README.md",
+        "Source Code": "https://github.com/Izak76/blomp-api",
+        "Bug Reports": "https://github.com/Izak76/blomp-api/issues",
+        "Changes": "https://github.com/Izak76/blomp-api/blob/main/CHANGELOG"
+    }
 )
